88caa53e01c541a6f61af76aefc99b102a6d5102
Greg Kostin <gkostin@umich.edu>
2025-08-22 14:08:50 -0400

    fmt and clippy

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,15 @@
-
-/// Search for a pattern in a file and display the lines that contain it.
-
+//! Search for a pattern in a file and display the lines that contain it.
 
 pub fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write) {
     for line in content.lines() {
         if line.contains(pattern) {
-            writeln!(writer, "{}", line).expect("could not write line");
+            writeln!(writer, "{line}").expect("could not write line");
         }
     }
 }
 
-
 #[cfg(test)]
 mod tests {
-    use super::*;
-
     #[test]
     #[ignore]
     fn another() {
