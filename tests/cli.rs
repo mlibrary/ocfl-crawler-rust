a903a1920fb2026c28ae1c681f6e98408f218fb7
Greg Kostin <gkostin@umich.edu>
2025-08-27 15:53:07 -0400

    test/cli/1.2 is not a storage root

--- tests/cli.rs
+++ tests/cli.rs
@@ -3,7 +3,7 @@ use assert_cmd::Command;
 use predicates::prelude::*;
 use pretty_assertions::assert_eq;
 use rand::{distr::Alphanumeric, Rng};
-use std::{borrow::Cow, fs, path::Path};
+use std::{borrow::Cow, fs};
 
 const PRG: &str = "ocfl-crawler-rust";
 
@@ -26,7 +26,7 @@ fn gen_bad_file() -> String {
 #[test]
 fn skips_bad_dir() -> Result<()> {
     let bad = gen_bad_file();
-    let expected = format!("{}: .* [(]os error [23][)]", &bad);
+    let expected = format!("{}.*is not a storage root", &bad);
     Command::cargo_bin(PRG)?
         .arg(&bad)
         .assert()
@@ -70,202 +70,213 @@ fn run(args: &[&str], expected_file: &str) -> Result<()> {
     let contents = fs::read_to_string(file.as_ref())?;
     let mut expected: Vec<&str> =
         contents.split('\n').filter(|s| !s.is_empty()).collect();
-    expected.sort();
+    // expected.sort();
 
     let cmd = Command::cargo_bin(PRG)?.args(args).assert().success();
     let out = cmd.get_output();
     let stdout = String::from_utf8(out.stdout.clone())?;
+    let stderr = String::from_utf8(out.stderr.clone())?;
     let mut lines: Vec<&str> =
         stdout.split('\n').filter(|s| !s.is_empty()).collect();
-    lines.sort();
+    lines.append(&mut stderr.split('\n').filter(|s| !s.is_empty()).collect());
+    // lines.sort();
 
     assert_eq!(lines, expected);
 
     Ok(())
 }
 
-// --------------------------------------------------
-#[test]
-fn path1() -> Result<()> {
-    run(&["tests/cli/inputs"], "tests/cli/expected/path1.txt")
-}
 
 // --------------------------------------------------
 #[test]
-fn path_a() -> Result<()> {
-    run(&["tests/cli/inputs/a"], "tests/cli/expected/path_a.txt")
-}
-
-// --------------------------------------------------
-#[test]
-fn path_a_b() -> Result<()> {
-    run(&["tests/cli/inputs/a/b"], "tests/cli/expected/path_a_b.txt")
-}
-
-// --------------------------------------------------
-#[test]
-fn path_d() -> Result<()> {
-    run(&["tests/cli/inputs/d"], "tests/cli/expected/path_d.txt")
-}
-
-// --------------------------------------------------
-#[test]
-fn type_f() -> Result<()> {
-    run(&["tests/cli/inputs", "-t", "f"], "tests/cli/expected/type_f.txt")
-}
-
-// --------------------------------------------------
-#[test]
-fn type_f_path_a() -> Result<()> {
-    run(
-        &["tests/cli/inputs/a", "-t", "f"],
-        "tests/cli/expected/type_f_path_a.txt",
-    )
-}
-
-// --------------------------------------------------
-#[test]
-fn type_f_path_a_b() -> Result<()> {
-    run(
-        &["tests/cli/inputs/a/b", "--type", "f"],
-        "tests/cli/expected/type_f_path_a_b.txt",
-    )
-}
-
-// --------------------------------------------------
-#[test]
-fn type_f_path_d() -> Result<()> {
-    run(
-        &["tests/cli/inputs/d", "--type", "f"],
-        "tests/cli/expected/type_f_path_d.txt",
-    )
-}
-
-// --------------------------------------------------
-#[test]
-fn type_d() -> Result<()> {
-    run(&["tests/cli/inputs", "-t", "d"], "tests/cli/expected/type_d.txt")
-}
-
-// --------------------------------------------------
-#[test]
-fn type_d_path_a() -> Result<()> {
-    run(
-        &["tests/cli/inputs/a", "-t", "d"],
-        "tests/cli/expected/type_d_path_a.txt",
-    )
-}
-
-// --------------------------------------------------
-#[test]
-fn type_d_path_a_b() -> Result<()> {
-    run(
-        &["tests/cli/inputs/a/b", "--type", "d"],
-        "tests/cli/expected/type_d_path_a_b.txt",
-    )
-}
-
-// --------------------------------------------------
-#[test]
-fn type_d_path_d() -> Result<()> {
-    run(
-        &["tests/cli/inputs/d", "--type", "d"],
-        "tests/cli/expected/type_d_path_d.txt",
-    )
-}
-
-// --------------------------------------------------
-#[test]
-fn type_l() -> Result<()> {
-    run(&["tests/cli/inputs", "-t", "l"], "tests/cli/expected/type_l.txt")
-}
-
-// --------------------------------------------------
-#[test]
-fn type_f_l() -> Result<()> {
-    run(
-        &["tests/cli/inputs", "-t", "l", "f"],
-        "tests/cli/expected/type_f_l.txt",
-    )
-}
-
-// --------------------------------------------------
-#[test]
-fn name_csv() -> Result<()> {
-    run(
-        &["tests/cli/inputs", "-n", ".*[.]csv"],
-        "tests/cli/expected/name_csv.txt",
-    )
-}
-
-// --------------------------------------------------
-#[test]
-fn name_csv_mp3() -> Result<()> {
-    run(
-        &["tests/cli/inputs", "-n", ".*[.]csv", "-n", ".*[.]mp3"],
-        "tests/cli/expected/name_csv_mp3.txt",
-    )
+fn storage_roots() -> Result<()> {
+    run(&["tests/cli/1.0", "tests/cli/1.1", "tests/cli/1.2"], "tests/cli/expected/storage_roots.txt")
 }
 
 
-// --------------------------------------------------
-#[test]
-fn name_a() -> Result<()> {
-    run(&["tests/cli/inputs", "-n", "a"], "tests/cli/expected/name_a.txt")
-}
-
-// --------------------------------------------------
-#[test]
-fn type_f_name_a() -> Result<()> {
-    run(
-        &["tests/cli/inputs", "-t", "f", "-n", "a"],
-        "tests/cli/expected/type_f_name_a.txt",
-    )
-}
-
-// --------------------------------------------------
-#[test]
-fn type_d_name_a() -> Result<()> {
-    run(
-        &["tests/cli/inputs", "--type", "d", "--name", "a"],
-        "tests/cli/expected/type_d_name_a.txt",
-    )
-}
-
-// --------------------------------------------------
-#[test]
-fn path_g() -> Result<()> {
-    run(&["tests/cli/inputs/g.csv"], "tests/cli/expected/path_g.txt")
-}
-
-// --------------------------------------------------
-#[test]
-#[cfg(not(windows))]
-fn unreadable_dir() -> Result<()> {
-    let dirname = "tests/cli/inputs/cant-touch-this";
-    if !Path::new(dirname).exists() {
-        fs::create_dir(dirname)?;
-    }
-
-    std::process::Command::new("chmod")
-        .args(["000", dirname])
-        .status()
-        .expect("failed");
-
-    let cmd = Command::cargo_bin(PRG)?
-        .arg("tests/cli/inputs")
-        .assert()
-        .success();
-    fs::remove_dir(dirname)?;
-
-    let out = cmd.get_output();
-    let stdout = String::from_utf8(out.stdout.clone())?;
-    let lines: Vec<&str> =
-        stdout.split('\n').filter(|s| !s.is_empty()).collect();
-
-    assert_eq!(lines.len(), 17);
-
-    let stderr = String::from_utf8(out.stderr.clone())?;
-    assert!(stderr.contains("cant-touch-this: Permission denied"));
-    Ok(())
-}
+//
+// // --------------------------------------------------
+// #[test]
+// fn path1() -> Result<()> {
+//     run(&["tests/cli/inputs"], "tests/cli/expected/path1.txt")
+// }
+//
+// // --------------------------------------------------
+// #[test]
+// fn path_a() -> Result<()> {
+//     run(&["tests/cli/inputs/a"], "tests/cli/expected/path_a.txt")
+// }
+//
+// // --------------------------------------------------
+// #[test]
+// fn path_a_b() -> Result<()> {
+//     run(&["tests/cli/inputs/a/b"], "tests/cli/expected/path_a_b.txt")
+// }
+//
+// // --------------------------------------------------
+// #[test]
+// fn path_d() -> Result<()> {
+//     run(&["tests/cli/inputs/d"], "tests/cli/expected/path_d.txt")
+// }
+//
+// // --------------------------------------------------
+// #[test]
+// fn type_f() -> Result<()> {
+//     run(&["tests/cli/inputs", "-t", "f"], "tests/cli/expected/type_f.txt")
+// }
+//
+// // --------------------------------------------------
+// #[test]
+// fn type_f_path_a() -> Result<()> {
+//     run(
+//         &["tests/cli/inputs/a", "-t", "f"],
+//         "tests/cli/expected/type_f_path_a.txt",
+//     )
+// }
+//
+// // --------------------------------------------------
+// #[test]
+// fn type_f_path_a_b() -> Result<()> {
+//     run(
+//         &["tests/cli/inputs/a/b", "--type", "f"],
+//         "tests/cli/expected/type_f_path_a_b.txt",
+//     )
+// }
+//
+// // --------------------------------------------------
+// #[test]
+// fn type_f_path_d() -> Result<()> {
+//     run(
+//         &["tests/cli/inputs/d", "--type", "f"],
+//         "tests/cli/expected/type_f_path_d.txt",
+//     )
+// }
+//
+// // --------------------------------------------------
+// #[test]
+// fn type_d() -> Result<()> {
+//     run(&["tests/cli/inputs", "-t", "d"], "tests/cli/expected/type_d.txt")
+// }
+//
+// // --------------------------------------------------
+// #[test]
+// fn type_d_path_a() -> Result<()> {
+//     run(
+//         &["tests/cli/inputs/a", "-t", "d"],
+//         "tests/cli/expected/type_d_path_a.txt",
+//     )
+// }
+//
+// // --------------------------------------------------
+// #[test]
+// fn type_d_path_a_b() -> Result<()> {
+//     run(
+//         &["tests/cli/inputs/a/b", "--type", "d"],
+//         "tests/cli/expected/type_d_path_a_b.txt",
+//     )
+// }
+//
+// // --------------------------------------------------
+// #[test]
+// fn type_d_path_d() -> Result<()> {
+//     run(
+//         &["tests/cli/inputs/d", "--type", "d"],
+//         "tests/cli/expected/type_d_path_d.txt",
+//     )
+// }
+//
+// // --------------------------------------------------
+// #[test]
+// fn type_l() -> Result<()> {
+//     run(&["tests/cli/inputs", "-t", "l"], "tests/cli/expected/type_l.txt")
+// }
+//
+// // --------------------------------------------------
+// #[test]
+// fn type_f_l() -> Result<()> {
+//     run(
+//         &["tests/cli/inputs", "-t", "l", "f"],
+//         "tests/cli/expected/type_f_l.txt",
+//     )
+// }
+//
+// // --------------------------------------------------
+// #[test]
+// fn name_csv() -> Result<()> {
+//     run(
+//         &["tests/cli/inputs", "-n", ".*[.]csv"],
+//         "tests/cli/expected/name_csv.txt",
+//     )
+// }
+//
+// // --------------------------------------------------
+// #[test]
+// fn name_csv_mp3() -> Result<()> {
+//     run(
+//         &["tests/cli/inputs", "-n", ".*[.]csv", "-n", ".*[.]mp3"],
+//         "tests/cli/expected/name_csv_mp3.txt",
+//     )
+// }
+//
+//
+// // --------------------------------------------------
+// #[test]
+// fn name_a() -> Result<()> {
+//     run(&["tests/cli/inputs", "-n", "a"], "tests/cli/expected/name_a.txt")
+// }
+//
+// // --------------------------------------------------
+// #[test]
+// fn type_f_name_a() -> Result<()> {
+//     run(
+//         &["tests/cli/inputs", "-t", "f", "-n", "a"],
+//         "tests/cli/expected/type_f_name_a.txt",
+//     )
+// }
+//
+// // --------------------------------------------------
+// #[test]
+// fn type_d_name_a() -> Result<()> {
+//     run(
+//         &["tests/cli/inputs", "--type", "d", "--name", "a"],
+//         "tests/cli/expected/type_d_name_a.txt",
+//     )
+// }
+//
+// // --------------------------------------------------
+// #[test]
+// fn path_g() -> Result<()> {
+//     run(&["tests/cli/inputs/g.csv"], "tests/cli/expected/path_g.txt")
+// }
+//
+// // --------------------------------------------------
+// #[test]
+// #[cfg(not(windows))]
+// fn unreadable_dir() -> Result<()> {
+//     let dirname = "tests/cli/inputs/cant-touch-this";
+//     if !Path::new(dirname).exists() {
+//         fs::create_dir(dirname)?;
+//     }
+//
+//     std::process::Command::new("chmod")
+//         .args(["000", dirname])
+//         .status()
+//         .expect("failed");
+//
+//     let cmd = Command::cargo_bin(PRG)?
+//         .arg("tests/cli/inputs")
+//         .assert()
+//         .success();
+//     fs::remove_dir(dirname)?;
+//
+//     let out = cmd.get_output();
+//     let stdout = String::from_utf8(out.stdout.clone())?;
+//     let lines: Vec<&str> =
+//         stdout.split('\n').filter(|s| !s.is_empty()).collect();
+//
+//     assert_eq!(lines.len(), 17);
+//
+//     let stderr = String::from_utf8(out.stderr.clone())?;
+//     assert!(stderr.contains("cant-touch-this: Permission denied"));
+//     Ok(())
+// }
